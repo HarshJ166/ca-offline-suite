207a088ca658fdaff5a435b4b8d4356a7bb20c6d
Raj Singh <rajsingh08471@gmail.com>
2025-01-24 15:25:36 +0530

    changes

--- frontend/db/db.js
+++ frontend/db/db.js
@@ -1,5 +1,17 @@
-require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") }); // Explicitly resolve the .env path
+const log = require("electron-log");
+const path = require("path");
+const { exec } = require("child_process");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const drizzleConfigPath = path.resolve(__dirname, "../drizzle.config.js");
+log.info('drizzleConfigPath', drizzleConfigPath);
+
+
+
+const isDev = process.env.NODE_ENV === "development";
+log.info('process.env.NODE_ENV', process.env.NODE_ENV);
+log.info('DB process.env.DB_FILE_NAME', process.env.DB_FILE_NAME);
 const { drizzle } = require("drizzle-orm/libsql");
+
 // const { createClient } = require("@libsql/client");
 // const { schema } = require("./schema");
 
@@ -8,7 +20,27 @@ class DatabaseManager {
 
   static getInstance() {
     if (!DatabaseManager.instance) {
+      // log.info("***********\n\n\nDatabasemanager instance is \n\n\n***********", DatabaseManager.instance);
+      if (!isDev) {
+        // Command to run migration
+        const command = `npx drizzle-kit migrate --config ${drizzleConfigPath}`;
+
+        // Execute the command
+        exec(command, (error, stdout, stderr) => {
+          if (error) {
+            log.error(`Error while running migrations: ${error.message}`);
+            return;
+          }
+          if (stderr) {
+            log.error(`Migration STDERR: ${stderr}`);
+            return;
+          }
+          log.info(`Migration STDOUT: ${stdout}`);
+        });
+      }
+
       const dbUrl = process.env.DB_FILE_NAME;
+
       console.log('dbUrl', dbUrl);
       if (!dbUrl) {
         throw new Error("DATABASE_URL is not defined in the environment variables.");
